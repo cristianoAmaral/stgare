d9d36368dfd1f4fad04ec335c78f694322ffd88d
cristianoAmaral <silvaamaralc@gmail.com>
2024-08-02 17:42:06 -0300

    Limpeza de arquivos duplicados

--- python_do_jeito_certo/2_exercicio.py
+++
@@ -1,6 +0,0 @@
-# Um comentário, assim você pode ler seu programa mais tarde.
-# Qualquer coisa depois de # é ignorado pelo Python
-print("I could have code likes this.") # e o comentário depois é ignorado
-# Você também pode usar um comentário para "Desabilitar" um código.
-# print("This won't runn.")
-print("This will run.")
